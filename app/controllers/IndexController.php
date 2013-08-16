e4ee70304207b87c1aba3170d26d7f66055d5a25
esemi <spam@esemi.ru>
2013-08-16 02:30:44 +0000

    Using keywords/title methods

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,7 +4,9 @@ class IndexController extends Zend_Controller_Action
 {
 	public function indexAction()
 	{
-
+		$this->view->headMeta()->appendName('keywords', 'оттава ё, главная, фолк, музыка, группа');
+		$this->view->headMeta()->appendName('description', 'Описание этой вашей группы.');
+		$this->view->headTitle('Главная страница');
 	}
 
 	public function bandAction()
