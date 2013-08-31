46d6b178559dcab04569960ffed8e6ae64910c0e
esemi <spam@esemi.ru>
2013-08-31 00:12:49 +0000

    Audio player src

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,22 @@
 /**
  * Main js file
  */
+
+$(document).ready(function(){
+
+	/**
+	 * jPlayer on main template
+	 */
+	console.log($("#jquery_jplayer_1"));
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function(){
+			$(this).jPlayer("setMedia", {
+				mp3: "/media/d464efc8a6963167a66667e27875ff2c_1377176502.mp3"
+			});
+		},
+		swfPath: "/js/jPlayer.2.4.0/",
+		supplied: "mp3"
+	});
+
+
+});
