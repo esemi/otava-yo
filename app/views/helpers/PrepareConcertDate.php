d79a5662c9827579f1fa00b4dfd363f4893f1bc1
esemi <spam@esemi.ru>
2013-09-18 22:39:47 +0400

    Add year to displayed date for news and concerts

--- app/views/helpers/PrepareConcertDate.php
+++ app/views/helpers/PrepareConcertDate.php
@@ -2,7 +2,7 @@
 
 class Zend_View_Helper_PrepareConcertDate extends Zend_View_Helper_Abstract
 {
-	public function prepareConcertDate($date)
+	public function prepareConcertDate(DateTime $date, $printYear = false, $time = '')
 	{
 		$months = array(
 			1 => 'янв',
@@ -19,7 +19,13 @@ class Zend_View_Helper_PrepareConcertDate extends Zend_View_Helper_Abstract
 			12 => 'дек'
 		);
 
-		$date = DateTime::createFromFormat('Y-m-d', $date);
-		return sprintf("%d<br>%s",$date->format('d'), $months[$date->format('n')]);;
+		$out = sprintf("%d<br>%s",$date->format('d'), $months[$date->format('n')]);
+		if( $printYear ){
+			$out .= sprintf('<div class="time">%s</div>', $date->format('Y'));
+		}else{
+			$out .= sprintf('<div class="time">%s</div>', $this->view->escape($time));
+		}
+		
+		return $out;
 	}
 }
