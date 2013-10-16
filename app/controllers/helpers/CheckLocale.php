bfde1862e5f9d5ccead990bd238be9f280821a72
esemi <spam@esemi.ru>
2013-10-17 02:58:28 +0400

    hotfix zend translate bug

--- app/controllers/helpers/CheckLocale.php
+++ app/controllers/helpers/CheckLocale.php
@@ -7,13 +7,13 @@ class Action_Helper_CheckLocale extends Zend_Controller_Action_Helper_Abstract
 	{
 		$avaliableLangs =  $this->getFrontController()->getParam('bootstrap')->getOption('locales');
 		$lang = $this->getRequest()->getParam('lang');
-
 		if( !in_array($lang, $avaliableLangs) ){
 			$lang = $avaliableLangs[0];
 		}
 
+		//ru-RU, ru, en-US, en
 		$oTranslate = $this->getActionController()->getInvokeArg('bootstrap')->getResource('Translate');
-		$oTranslate->getAdapter()->setOptions( array('log' => $this->getActionController()->getInvokeArg('bootstrap')->getResource('Log')) );
 		$oTranslate->getAdapter()->setLocale($lang);
+		$oTranslate->getAdapter()->setOptions( array('log' => $this->getActionController()->getInvokeArg('bootstrap')->getResource('Log')) );
 	}
 }
