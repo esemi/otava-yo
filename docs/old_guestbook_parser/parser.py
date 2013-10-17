56a025c43fffe1bc1cd9a55eccc215ed333e2132
esemi <spam@esemi.ru>
2013-10-17 03:40:57 +0000

    Complete parse all old guestbook fields

--- docs/old_guestbook_parser/parser.py
+++ docs/old_guestbook_parser/parser.py
@@ -3,6 +3,7 @@
 
 __author__ = 'esemi'
 
+from dateutil.parser import parse as date_parser
 import os
 import logging
 import re
@@ -10,8 +11,11 @@ import re
 
 CURRENT_PATH = os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
 
-AUTHOR_RE = re.compile(u'</b><br><a href="http://(.*)">(.+)</a>', re.U | re.I)
-SITE_RE = re.compile(u'</b><br><a href="http://(.+)/">', re.U | re.I)
+SITE_RE = re.compile(u'<a href="http[s]?://(.+)">(.+)</a>(\s&lt;|<br>)', re.U | re.I)
+EMAIL_RE = re.compile(u'&lt;<a href="mailto:(.+)">(.+)</a>&gt;', re.U | re.I)
+AUTHOR_RE = re.compile(u'(.+)&lt;|(.+)<br>', re.U | re.I)
+CITY_RE = re.compile(u'<br>(.+)\s-\s', re.U | re.I)
+DATE_RE = re.compile(u'\s-\s(.+)', re.U | re.I)
 
 
 def split_posts(content):
@@ -21,23 +25,51 @@ def split_posts(content):
 def parse_post(content):
 
     post = dict(
+        site='',
+        content='',
+        email='',
         author='',
         date_publish='',
-        email='',
-        city='',
-        site='',
-        content=''
+        city=''
     )
 
     parts = content.split('</b><br>')
     if len(parts) != 2:
         raise Exception('Invalid post parts count')
 
-    # parse content @TODO supporting <br> tag ?
-    post['content'] = re.sub(ur'<[^>]*?>', '', parts[0])
+    # parse content todo supporting <br> tag ?
+    post['content'] = re.sub(ur'<[^>]*?>', '', parts[0]).strip()
 
-    # parse author
+    # parse site
+    site = SITE_RE.search(parts[1].replace('<a href="http:///">', ''))
+    if site is not None:
+        post['site'] = site.group(1).replace('otava-yo.spb.ru/guestbook/', '').strip()
 
+    # parse email
+    email = EMAIL_RE.search(parts[1])
+    if email is not None:
+        post['email'] = email.group(1).strip()
+
+    # parse author todo replace аб to Отава Ё
+    author_from_site = SITE_RE.search(parts[1])
+    author_from_str = AUTHOR_RE.search(parts[1])
+    if author_from_site is not None:
+        post['author'] = author_from_site.group(2).strip()
+    elif author_from_str is not None:
+        post['author'] = filter(lambda x: x is not None, list(author_from_str.groups()))[0].strip()
+
+    # parse city
+    city = CITY_RE.search(parts[1])
+    if city is not None:
+        post['city'] = city.group(1).strip(' ,')
+
+    # parse date todo
+    date = DATE_RE.search(parts[1])
+    if date is not None:
+        post['date_publish'] = date.group(1).strip()
+        # todo parse date
+        print post['date_publish']
+        print date_parser(post['date_publish'])
 
     return post
 
@@ -57,8 +89,11 @@ if __name__ == '__main__':
         for num, post in enumerate(posts):
             logging.info('process %d post' % num)
             post = post.strip()
-            logging.debug(post)
-            logging.debug(parse_post(post)['content'])
+
+            parsed_post = parse_post(post)
+            logging.info(parsed_post)
+
+            # todo validate fields
 
 
 
