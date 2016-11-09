b754d95c1461c588521fef74a24eb4f10997a497
Semen Efremov <s.efremov@semrush.com>
2016-11-09 21:50:31 +0300

    add retargeting script

---
+++ www/js/retarget.js
@@ -0,0 +1,1 @@
+(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=Ig8VaQGz/EiGtAHeR3L*0t93BmUAEExMKI2PzvC751nBKmebQe5ZM3eo1ej8RPgXCLHunYGYMbLECG399X8CruE5fUxIEshNo31AumbvlnmH2H7l4GOSzPK8cGUtfAQLzNrdSMgER4cXMpir15HtM4ItCIuioFjYDihHsikleVg-&pixel_id=1000035742';
